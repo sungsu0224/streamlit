1b2f228bfb645d4241e1f776256a323cbd7e490f
Kim-sungsu <rlatjdtn1014@naver.com>
2024-05-15 14:34:51 +0900

    test

--- home.py
+++ home.py
@@ -9,6 +9,7 @@ st.write("This is a simple Streamlit app.")
 # 사용자 입력
 user_input = st.text_input("Enter some text:")
 
+st.text("her")
 # 입력된 텍스트 출력
 if user_input:
     st.write(f"You entered: {user_input}")
