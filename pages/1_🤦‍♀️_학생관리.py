2f3fffd567e059b7a1b653f2be0d090d527cc30e
sungsu0224 <rlatjdtn1014@naver.com>
2024-05-17 13:48:00 +0000

    bug gix

--- pages/1_🤦‍♀️_학생관리.py
+++ pages/1_🤦‍♀️_학생관리.py
@@ -5,7 +5,6 @@ from Dao import pymongo_users
 # 페이지 제목 및 설명
 st.title("학생관리 페이지")
 st.write("이 페이지를 사용하여 학생을 등록, 수정, 삭제 할 수 있습니다")
-st.experimental_get_query_params()
 
 with st.expander("신규 원생 등록") :
     name = st.text_input("학생이름")
