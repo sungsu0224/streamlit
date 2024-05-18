8b3c907589422d7053307603328f251096463110
sungsu0224 <rlatjdtn1014@naver.com>
2024-05-18 07:55:19 +0000

    annotated text import

--- home.py
+++ home.py
@@ -1,6 +1,23 @@
 import streamlit as st
 from Dao import pymongo_users
+from annotated_text import annotated_text
 # 제목
 st.title("피드백")
 
 # 텍스트
+annotated_text(
+    "This ",
+    ("is", "verb"),
+    " some ",
+    ("annotated", "adj"),
+    ("text", "noun"),
+    " for those of ",
+    ("you", "pronoun"),
+    " who ",
+    ("like", "verb"),
+    " this sort of ",
+    ("thing", "noun"),
+    "."
+)
+
+
--- pages/student.py
+++ pages/student.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import pandas as pd
+from annotated_text import annotated_text
 
 
 name = st.query_params["name"]
 
 
 # 페이지 제목 및 설명
+
 st.title("학생 피드백 페이지")
 
 st.write("이 페이지에서 학부모님들이 학생들의 피드백 결과를 볼 수 있습니다.")
